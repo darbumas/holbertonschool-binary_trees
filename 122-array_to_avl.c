33081d810f99c97e2e4b27832dd10e98e0ba1e3a
darbumas <2314@holbertonschool.com>
2023-03-15 19:48:10 +0000

    refactor

--- 122-array_to_avl.c
+++ 122-array_to_avl.c
@@ -9,23 +9,14 @@
 avl_t *array_to_avl(int *array, size_t size)
 {
 	avl_t *tree = NULL;
-	size_t i, j;
+	size_t i;
 
 	if (!array)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < i; j++)
-		{
-			if (array[j] == array[i])
-				break;
-		}
-		if (j == i)
-		{
-			if (avl_insert(&tree, array[i]) == NULL)
-				return (NULL);
-		}
+		avl_insert(&tree, array[i]);
 	}
 	return (tree);
 }
